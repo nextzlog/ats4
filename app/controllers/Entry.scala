eec7e2fb1a20c2943cd65f250115eb7bfe73ce4f
JG1VPP <jg1vpp@gmail.com>
2017-06-02 22:41:33 +0900

    debug

--- app/controllers/Entry.scala
+++ app/controllers/Entry.scala
@@ -74,8 +74,8 @@ class Entry @Inject()(smtp: MailerClient)(implicit db: Database) extends Control
 						val pmIsAllBands = Conf.sectsAllBands.contains(postPM.last.sect)
 						if(amIsAllBands && pmIsAllBands) {
 							if(postAM.last.city == postPM.last.city) {
-								val prefixAM = postAM.last.sect.split(" ").take(3)
-								val prefixPM = postPM.last.sect.split(" ").take(3)
+								val prefixAM = postAM.last.sect.split(" ").take(3).toList
+								val prefixPM = postPM.last.sect.split(" ").take(3).toList
 								if(prefixAM.take(2) == prefixPM.take(2)) {
 									val mode = (prefixAM.last, prefixPM.last) match {
 										case ("電信電話",_) => "電信電話"
