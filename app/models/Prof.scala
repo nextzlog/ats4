bece4b9d1f08cf4d7e5bf687b9eee3ba959b8113
JG1VPP <jg1vpp@gmail.com>
2017-06-04 00:17:33 +0900

    section simplified

--- app/models/Prof.scala
+++ app/models/Prof.scala
@@ -5,5 +5,6 @@ import scala.collection.JavaConversions._
 
 case class Prof(call: String, city: String, sect: String, name: String, addr: String, mail: String, comm: String) {
 	def safeCall = normalize(call.toUpperCase, Form.NFKC).split('/').head.replaceAll("[^0-9A-Z]", "") // prohibit directory traversal
-	def post(summ: qxsl.ruler.Summary) = Post(call, city, sect, name, addr, mail, comm, summ.calls, summ.mults)
+	def fullSect = "1エリア%s %s".format(if(Conf.inner.contains(qxsl.field.City.forName(city))) "内" else "外", sect)
+	def post(summ: qxsl.ruler.Summary) = Post(call, city, fullSect, name, addr, mail, comm, summ.calls, summ.mults)
 }
