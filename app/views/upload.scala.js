41c6a60591d659dff9ab456b8ce03446748ab85d
JG1VPP <jg1vpp@gmail.com>
2018-06-01 00:20:34 +0900

    upload.scala.html/js debug

--- app/views/upload.scala.js
+++ app/views/upload.scala.js
@@ -3,3 +3,7 @@
 $('#@id').change(function() {
 	$('#@{id}-name').html($(this).val().split(/[\\\/]+/).pop())
 })
+
+document.getElementById('@{id}-button').onclick = function() {
+	$('#@id').click();
+};
