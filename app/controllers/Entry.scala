9f574ee855216cc03103ba50910a0042b001b1b8
JG1VPP <jg1vpp@gmail.com>
2019-04-28 00:54:20 +0900

    Controller-refactoring

--- app/controllers/Entry.scala
+++ app/controllers/Entry.scala
@@ -1,30 +1,32 @@
 package controllers
 
-import java.io.{File, IOException => UnsupException}
-import java.util.{NoSuchElementException => OmissException}
-import javax.inject.Inject
-import models._
+import java.io.{IOException=>Unsup}
+import java.util.{NoSuchElementException=>Omiss}
+import javax.inject.{Inject,Singleton}
+import models.{Acceptor,DeadLine,Scaner}
 import play.api.Configuration
 import play.api.db.Database
-import play.api.mvc.{Action, InjectedController}
+import play.api.mvc.{Action,InjectedController}
 import play.libs.mailer.MailerClient
-import scala.util.Try
-import views.html.{pages, warns}
+import views.html.pages.{entry,index,proof}
+import views.html.warns.{omiss,unsup}
 
-class Entry @Inject() (implicit smtp: MailerClient, cfg: Configuration, db: Database) extends InjectedController {
-	def form = Action(implicit req => Ok(if(DeadLine.isOK) pages.entry(Scaner) else pages.index()))
-	def post = Action(parse.multipartFormData) (implicit req => Try {
-		val prof = Scaner.bindFromRequest.get
-		val file = req.body.file("eLog").get.ref
-		Ok(pages.proof(new Acceptor().accept(prof,file),None))
+@Singleton class Entry extends InjectedController {
+	@Inject implicit var smtp: MailerClient = null
+	@Inject implicit var cfg: Configuration = null
+	@Inject implicit var db: Database = null
+	def form = Action(implicit r=>if(DeadLine.isOK) {
+		Ok(entry(Scaner))
+	} else {
+		Gone(index())
+	})
+	def post = Action(implicit r=>util.Try {
+		val data = r.body.asMultipartFormData
+		val form = Scaner.bindFromRequest.get
+		val file = data.get.file("sheet").get
+		Ok(proof(new Acceptor().accept(form,file.ref)))
 	}.recover {
-		case ex: OmissException => {
-			play.api.Logger.error(warns.omiss().body, ex)
-			Ok(pages.entry(Scaner.bindFromRequest, Some(warns.omiss())))
-		}
-		case ex: UnsupException => {
-			play.api.Logger.error(warns.unsup().body, ex)
-			Ok(pages.entry(Scaner.bindFromRequest, Some(warns.unsup())))
-		}
+		case ex: Omiss => Ok(entry(Scaner.bindFromRequest, Some(omiss())))
+		case ex: Unsup => Ok(entry(Scaner.bindFromRequest, Some(unsup())))
 	}.get)
 }
