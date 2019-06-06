842cb2cce7638fd4c0bc99af609c27e92b2ae61e
JG1VPP <jg1vpp@gmail.com>
2019-06-06 23:09:40 +0900

    admin: revision & elimination support

--- app/controllers/Admin.scala
+++ app/controllers/Admin.scala
@@ -1,27 +1,24 @@
 package controllers
 
-import java.io.{File,IOException=>Unsup}
-import java.util.{NoSuchElementException=>Omiss}
+import java.io.File
 import javax.inject.{Inject,Singleton}
 import models.Record.forId
-import models.{Acceptor,Sections}
+import models.{Format,Sections}
 import play.api.Configuration
 import play.api.db.Database
 import play.api.mvc.{Action,InjectedController}
-import play.libs.mailer.MailerClient
 import scala.concurrent.ExecutionContext
-import views.html.pages.{index,lists,proof}
-import views.html.warns.{omiss,unsup}
+import views.html.pages.{entry,lists,proof}
 
 @Singleton class Admin extends InjectedController {
-	@Inject implicit var smtp: MailerClient = null
 	@Inject implicit var cfg: Configuration = null
 	@Inject implicit var db: Database = null
 	private implicit val admin = true
 	private implicit val ec = ExecutionContext.global
 	def root = Action(Ok(lists(Sections.all)))
-	def sums(id: Long) = Action(implicit r=>util.Try {
-		Ok(proof(forId(id).get))
+	def form(id: Long) = Action(implicit r=>util.Try {
+		val scaned = forId(id).get.scaned.copy(addr=null)
+		Ok(entry(Format.fill(scaned)))
 	}.getOrElse {
 		NotFound(lists(Sections.all))
 	})
@@ -30,13 +27,15 @@ import views.html.warns.{omiss,unsup}
 	}.getOrElse {
 		NotFound(lists(Sections.all))
 	})
-	def post(id: Long) = Action(implicit r=>util.Try {
-		val data = r.body.asMultipartFormData
-		val file = data.get.file("sheet").get
-		val scan = forId(id).get.scaned
-		Ok(proof(new Acceptor().accept(scan,file.ref)))
-	}.recover {
-		case ex: Omiss => Ok(proof(forId(id).get,Some(omiss())))
-		case ex: Unsup => Ok(proof(forId(id).get,Some(unsup())))
-	}.get)
+	def sums(id: Long) = Action(implicit r=>util.Try {
+		Ok(proof(forId(id).get))
+	}.getOrElse {
+		NotFound(lists(Sections.all))
+	})
+	def elim(id: Long) = Action(implicit r=>util.Try {
+		forId(id).get.purge
+		Ok(lists(Sections.all))
+	}.getOrElse {
+		NotFound(lists(Sections.all))
+	})
 }
