40ada233bff67d0442adbb5be0b58e93635c9d3d
JG1VPP <jg1vpp@gmail.com>
2019-06-17 18:50:14 +0900

    client/server error handler

---
+++ app/controllers/Error.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import javax.inject.{Inject,Singleton}
+import play.api.Configuration
+import play.api.http.HttpErrorHandler
+import play.api.mvc.RequestHeader
+import play.api.mvc.Results.{InternalServerError,Status}
+import scala.concurrent.Future
+import views.html.pages.index
+
+@Singleton class Error extends HttpErrorHandler {
+	@Inject implicit var cfg: Configuration = null
+	override def onClientError(r: RequestHeader, s: Int, msg: String) = Future.successful(Status(s)(index(Some(s))))
+	override def onServerError(r: RequestHeader, error: Throwable) = Future.successful(InternalServerError(index()))
+}
