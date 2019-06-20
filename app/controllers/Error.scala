30a0c625184d03ed5664dd1e7ce195a805d9f2bd
JG1VPP <jg1vpp@gmail.com>
2019-06-20 14:13:39 +0900

    index: comment view

--- app/controllers/Error.scala
+++ app/controllers/Error.scala
@@ -2,6 +2,7 @@ package controllers
 
 import javax.inject.{Inject,Singleton}
 import play.api.Configuration
+import play.api.db.Database
 import play.api.http.HttpErrorHandler
 import play.api.mvc.RequestHeader
 import play.api.mvc.Results.{InternalServerError,Status}
@@ -10,6 +11,7 @@ import views.html.pages.index
 
 @Singleton class Error extends HttpErrorHandler {
 	@Inject implicit var cfg: Configuration = null
+	@Inject implicit var db: Database = null
 	override def onClientError(r: RequestHeader, s: Int, msg: String) = Future.successful(Status(s)(index(Some(s))))
 	override def onServerError(r: RequestHeader, error: Throwable) = Future.successful(InternalServerError(index()))
 }
