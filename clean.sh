a30849e74e3b655030a53b9371742204e8c04216
JG1VPP <jg1vpp@gmail.com>
2020-07-02 19:01:40 +0900

    clean.sh: more safe

--- clean.sh
+++ clean.sh
@@ -1,6 +1,15 @@
-sbt clean
-rm *.db
-rm -r logs
-rm -r target
-rm -r project/project
-rm -r project/target
+read -p "Do you really want to delete all database records? (y/n):" YN
+if [ "${YN}" = "y" ]
+then
+	if [ $(pgrep java) ];
+	then
+		echo "A Java process is running, abort."
+	else
+		sbt update clean
+		rm *.db
+		rm -r logs
+		rm -r target
+		rm -r project/project
+		rm -r project/target
+	fi
+fi
