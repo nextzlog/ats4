6a30f11f258bed80db080265dda7b59bfba10efb
JG1VPP <jg1vpp@gmail.com>
2022-06-27 20:10:21 +0900

    CSRF token exception

--- app/controllers/Entry.scala
+++ app/controllers/Entry.scala
@@ -13,15 +13,12 @@ import views.html.{pages, warns}
 import org.apache.commons.mail.EmailException
 
 @Singleton class Entry @Inject()(implicit smtp: MailerClient) extends InjectedController {
-	class TokenException extends Exception
-	def entry = Action(implicit r => util.Try {
-		if(CSRF.getToken.isEmpty) throw new TokenException
+	def entry = Action(implicit r => if(CSRF.getToken.isDefined) util.Try {
 		val data = r.body.asMultipartFormData
 		val form = ContestForm.bindFromRequest()
 		Ok(pages.proof(new Acceptor().push(post = form.get, files = data.get.files.map(_.ref))))
 	}.recover {
 		case ex: OmissException => Ok(pages.entry(ContestForm.bindFromRequest(), Some(warns.omiss())))
 		case ex: EmailException => Ok(pages.entry(ContestForm.bindFromRequest(), Some(warns.email())))
-		case ex: TokenException => Ok(pages.entry(ContestForm.bindFromRequest(), Some(warns.token())))
-	}.get)
+	}.get else Ok(pages.entry(ContestForm, Some(warns.token()))))
 }
