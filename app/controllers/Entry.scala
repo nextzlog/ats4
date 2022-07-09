af2216951912cb2d6524026050de036e46eb3ab6
JG1VPP <jg1vpp@gmail.com>
2022-07-10 01:14:59 +0900

    log EmailException

--- app/controllers/Entry.scala
+++ app/controllers/Entry.scala
@@ -1,6 +1,6 @@
 package controllers
 
-import java.util.{NoSuchElementException => OmissException}
+import java.util.NoSuchElementException
 import javax.inject.{Inject, Singleton}
 
 import play.api.mvc.InjectedController
@@ -10,15 +10,12 @@ import play.libs.mailer.MailerClient
 import models.{Acceptor, ContestForm}
 import views.html.{pages, warns}
 
-import org.apache.commons.mail.EmailException
-
 @Singleton class Entry @Inject()(implicit smtp: MailerClient) extends InjectedController {
 	def entry = Action(implicit r => if(CSRF.getToken.isDefined) util.Try {
 		val data = r.body.asMultipartFormData
 		val form = ContestForm.bindFromRequest()
 		Ok(pages.proof(new Acceptor().push(post = form.get, files = data.get.files.map(_.ref))))
 	}.recover {
-		case ex: OmissException => Ok(pages.entry(ContestForm.bindFromRequest(), Some(warns.omiss())))
-		case ex: EmailException => Ok(pages.entry(ContestForm.bindFromRequest(), Some(warns.email())))
+		case ex: NoSuchElementException => Ok(pages.entry(ContestForm.bindFromRequest(), Some(warns.omiss())))
 	}.get else Ok(warns.token()))
 }
--- app/models/mail.scala
+++ app/models/mail.scala
@@ -1,10 +1,13 @@
 package models
 
+import play.api.Logger
 import play.libs.mailer.{Email, MailerClient}
+
+import org.apache.commons.mail.EmailException
 
 class SendMail(implicit smtp: MailerClient) {
 	def remind(call: String) = StationData.findAllByCall(call).map(send).mkString
-	def send(station: StationData) = {
+	def send(station: StationData) = util.Try {
 		val mail = new Email
 		val text = views.txt.pages.email(station.call).body.trim
 		mail.setFrom("%s <%s>".format(Rule.rule.host, Rule.rule.mail))
@@ -13,6 +16,7 @@ class SendMail(implicit smtp: MailerClient) {
 		mail.setSubject(text.linesIterator.toSeq.head.split(";").head.trim)
 		mail.setBodyText(text.linesWithSeparators.toSeq.tail.mkString.trim)
 		smtp.send(mail)
-		"OK"
+	}.recover {
+		case ex: EmailException => Logger(classOf[SendMail]).error("MAIL ERROR!", ex)
 	}
 }
