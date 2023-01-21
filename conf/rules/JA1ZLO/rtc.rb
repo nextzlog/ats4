b43fe042eb4303ebed0cf407d0e7d88a7b333e9c
JG1VPP <jg1vpp@gmail.com>
2023-01-21 23:14:42 +0900

    qxsl.ruler.Program.Annual

--- conf/rules/JA1ZLO/rtc.rb
+++ conf/rules/JA1ZLO/rtc.rb
@@ -1,65 +1,58 @@
-# REAL-TIME CONONLINE DEFINED by ATS-4
+# REAL-TIME CONONLINE DEFINED for ATS-4
 
 java_import 'java.time.DayOfWeek'
-java_import 'java.time.LocalDate'
-java_import 'java.time.Year'
-java_import 'java.time.temporal.TemporalAdjusters'
 java_import 'qxsl.ruler.Program'
 java_import 'qxsl.ruler.RuleKit'
 java_import 'qxsl.utils.AssetUtil'
 
-def schedule(year, month, nth, dayOfWeek)
-	week = DayOfWeek.valueOf(dayOfWeek)
-	date = LocalDate.of(year, month, 1)
-	date.with(TemporalAdjusters.dayOfWeekInMonth(nth, week))
-end
-
-def opt_year(func_start_day, months = 9)
-	year = Year.now.getValue
-	date = func_start_day.call(year)
-	span = date.until(LocalDate.now)
-	(span.getMonths > months ? 1: 0) + year
-end
-
 ONLINE = RuleKit.load('/rules/JA1ZLO/rtc.lisp').contest
 
-class ProgramRTC < Program
+class ProgramRTC < Program::Annual
+	def initialize()
+		super(7, 4, DayOfWeek::MONDAY)
+		ONLINE.each{|section| add(section)}
+	end
+
 	def name()
 		'リアルタイムコンテスト'
 	end
+
 	def host()
 		'東大無線部'
 	end
+
 	def mail()
 		'allja1@ja1zlo.u-tokyo.org'
 	end
+
 	def link()
 		'ja1zlo.u-tokyo.org/rt'
 	end
+
 	def help()
 		AssetUtil.root.string('rules/JA1ZLO/rtc.md')
 	end
+
 	def get(name)
 		eval name
 	end
-	def year()
-		opt_year(method(:getStartDay))
-	end
-	def getStartDay(year)
-		schedule(year, 7, 4, 'MONDAY')
-	end
+
 	def getFinalDay(year)
-		schedule(year, 12, 4, 'MONDAY')
+		getStartDay(year).plusMonths(5)
 	end
+
 	def getDeadLine(year)
-		schedule(year, 12, 4, 'MONDAY')
+		getStartDay(year).plusMonths(5)
 	end
+
 	def finish(zone)
 		true
 	end
+
 	def limitMultipleEntry(code)
 		1
 	end
+
 	def conflict(entries)
 		mul = entries.map{|e| e.name.include?("団体")}.any?
 		sin = entries.map{|e| e.name.include?("個人")}.any?
@@ -68,4 +61,4 @@ class ProgramRTC < Program
 end
 
 # returns contest definition
-ProgramRTC.new(*ONLINE)
+ProgramRTC.new
--- conf/rules/JI1YEG/tama.rb
+++ conf/rules/JI1YEG/tama.rb
@@ -1,10 +1,8 @@
-# TAMAGAWA CONTEST DEFINED by ATS-4
+# TAMAGAWA CONTEST DEFINED for ATS-4
 
 java_import 'java.time.DayOfWeek'
-java_import 'java.time.LocalDate'
-java_import 'java.time.Year'
 java_import 'java.time.ZoneId'
-java_import 'java.time.temporal.TemporalAdjusters'
+java_import 'qxsl.draft.Mode'
 java_import 'qxsl.draft.Qxsl'
 java_import 'qxsl.local.LocalCityBase'
 java_import 'qxsl.ruler.Element'
@@ -15,26 +13,11 @@ java_import 'qxsl.ruler.Section'
 java_import 'qxsl.ruler.Success'
 java_import 'qxsl.utils.AssetUtil'
 
-def schedule(year, month, nth, dayOfWeek)
-	week = DayOfWeek.valueOf(dayOfWeek)
-	date = LocalDate.of(year, month, 1)
-	date.with(TemporalAdjusters.dayOfWeekInMonth(nth, week))
-end
-
-def opt_year(func_start_day, months = 9)
-	year = Year.now.getValue
-	date = func_start_day.call(year)
-	span = date.until(LocalDate.now)
-	(span.getMonths > months ? 1: 0) + year
-end
-
 # JAUTIL library
 JAUTIL = RuleKit.load('jautil.lisp').pattern
 ZONEID = ZoneId.of('Asia/Tokyo')
 
-HOURDB = [13, 14]
-BANDDB = [50_000]
-CITYDB = LocalCityBase.load('rules/JI1YEG/tama.dat').toList
+TAMAs = LocalCityBase.load('rules/JI1YEG/tama.dat').toList
 
 module ModeEnum
 	MORSE = ['CW']
@@ -44,69 +27,47 @@ end
 SCORES = {'CW' => 3}
 SCORES.default = 2
 
-def verify_time(time)
-	HOURDB.include?(time.withZoneSameInstant(ZONEID).getHour)
-end
+class ProgramTama < Program::Annual
+	def initialize()
+		super(11, 4, DayOfWeek::SUNDAY)
+	end
 
-def verify_code(code)
-	CITYDB.any?{|c| c.code == code}
-end
-
-def verify_item(item, modeDB)
-	time = item.value(Qxsl::TIME)
-	call = item.value(Qxsl::CALL)
-	band = item.value(Qxsl::BAND).intValue
-	mode = item.value(Qxsl::MODE)
-	code = item.getRcvd.value(Qxsl::CODE)
-	return Failure.new(item, 'bad time') if not verify_time(time)
-	return Failure.new(item, 'bad code') if not verify_code(code)
-	return Failure.new(item, 'bad band') if not BANDDB.include?(band)
-	return Failure.new(item, 'bad mode') if not modeDB.include?(mode)
-	return Success.new(item, SCORES[mode])
-end
-
-def unique_item(item)
-	Element.new(item.value(Qxsl::CALL))
-end
-
-def entity_item(item)
-	Element.new(item.getRcvd.value(Qxsl::CODE))
-end
-
-class ProgramTama < Program
 	def name()
 		'多摩川コンテスト'
 	end
+
 	def host()
 		'APOLLO'
 	end
+
 	def mail()
 		'jk1mgc@example.com'
 	end
+
 	def link()
 		'apollo.c.ooco.jp'
 	end
+
 	def help()
 		AssetUtil.root.string('rules/JI1YEG/tama.md')
 	end
+
 	def get(name)
 		eval name
 	end
-	def year()
-		opt_year(method(:getStartDay))
-	end
-	def getStartDay(year)
-		schedule(year, 11, 4, 'SUNDAY')
-	end
+
 	def getFinalDay(year)
-		schedule(year, 11, 4, 'SUNDAY')
+		getStartDay(year)
 	end
+
 	def getDeadLine(year)
-		start_day(year).plusWeeks(2)
+		getStartDay(year).plusWeeks(2)
 	end
+
 	def limitMultipleEntry(code)
 		1
 	end
+
 	def conflict(entries)
 		entries.length > 1
 	end
@@ -116,26 +77,63 @@ class SectionTama < Section
 	def initialize(name, mode)
 		super()
 		@name = name
-		@mode = mode
+		@band = [Band.new(50_000)]
+		@mode = mode.map{|m| Mode.new(m)}
+		@hour = [13, 14]
 	end
+
 	def name()
 		@name
 	end
+
 	def code()
 		'TAMA'
 	end
+
 	def getCityList()
-		CITYDB
+		TAMAs
 	end
+
 	def verify(item)
-		verify_item(JAUTIL.normalize(item, nil), @mode)
+		errors = []
+		item = JAUTIL.normalize(item, @name)
+		errors.push verify_time(item.getBoth(Qxsl::TIME))
+		errors.push verify_band(item.getBoth(Qxsl::BAND))
+		errors.push verify_mode(item.getBoth(Qxsl::MODE))
+		errors.push verify_code(item.getRcvd(Qxsl::CODE))
+		return Success.new(item, points(item)) if errors.all? ''
+		return Failure.new(item, errors.grep_v('').join(', '))
 	end
+
+	def verify_time(time)
+		hour = time.value.withZoneSameInstant(ZONEID).getHour
+		@hour.include?(hour)? '': "hour: #{hour}"
+	end
+
+	def verify_band(band)
+		@band.include?(band)? '': "band: #{band}"
+	end
+
+	def verify_mode(mode)
+		@mode.include?(mode)? '': "mode: #{mode}"
+	end
+
+	def verify_code(code)
+		TAMAs.include?(code)? '': "code: #{code}"
+	end
+
+	def points(mode)
+		SCORES[item.value(Qxsl::MODE)]
+	end
+
 	def unique(item)
-		unique_item(item)
+		Element.new(item.value(Qxsl::CALL))
 	end
+
 	def entity(item)
-		entity_item(item)
+		Element.new(item.getRcvd.value(Qxsl::CODE))
 	end
+
 	def result(list)
 		score,mults = list.toArray
 		score > 0? score * mults.size: 0
@@ -148,12 +146,12 @@ SWLER = 'SWL'
 MORSE = '電信'
 PHONE = '電信電話'
 
-LIST = []
-LIST.push SectionTama.new(INNER + MORSE, ModeEnum::MORSE)
-LIST.push SectionTama.new(INNER + PHONE, ModeEnum::PHONE)
-LIST.push SectionTama.new(OUTER + MORSE, ModeEnum::MORSE)
-LIST.push SectionTama.new(OUTER + PHONE, ModeEnum::PHONE)
-LIST.push SectionTama.new(SWLER,         ModeEnum::PHONE)
+RULE = ProgramTama.new
+RULE.add(SectionTama.new(INNER + MORSE, ModeEnum::MORSE))
+RULE.add(SectionTama.new(INNER + PHONE, ModeEnum::PHONE))
+RULE.add(SectionTama.new(OUTER + MORSE, ModeEnum::MORSE))
+RULE.add(SectionTama.new(OUTER + PHONE, ModeEnum::PHONE))
+RULE.add(SectionTama.new(SWLER,         ModeEnum::PHONE))
 
 # returns contest definition
-ProgramTama.new(*LIST)
+RULE
