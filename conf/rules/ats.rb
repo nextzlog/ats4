a7e761aac1370301f2652682b4f267c45afc6e86
JG1VPP <jg1vpp@gmail.com>
2024-06-14 00:20:18 +0900

    cache SectionATS.loadDAT(url)

--- conf/rules/ats.rb
+++ conf/rules/ats.rb
@@ -86,6 +86,8 @@ class AbsenceATS < Absence
 end
 
 class SectionATS < Section
+	@@DATs = {}
+
 	def initialize(name, band, mode, hour = 0..23, zdat = nil)
 		super()
 		@name = name
@@ -156,9 +158,12 @@ class SectionATS < Section
 
 	def loadDAT(url)
 		return JAUTIL.get('CITY-BASE') if url.nil?
+		return @@DATs.fetch(url) if @@DATs.include?(url)
 		text = AssetUtil.root.http(URL.new(url), 'SJIS')
-		text = text.gsub(/^(\w+) +(.+)$/, '\2 \1')
-		LocalCityBase.read(text.lines[1..-2].join)
+		text = text.gsub(/^(\w+) +(.+)$/, '\2 \1').lines.drop(1)
+		base = LocalCityBase.read(text.take(text.size - 1).join)
+		@@DATs[url] = base
+		base
 	end
 end
 
